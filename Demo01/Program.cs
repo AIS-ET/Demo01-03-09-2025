1ec3faae578eed1ebb054786dea553d60ffe7d6b
SSS <schifa.disa>
2025-09-03 09:04:33 -0500

    Saved 1

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Demo01
+class Ejercicio1
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        // Ejercicio 1
+        // Pago total por alquiler de una casa
+        // Desarrollar un programa que calcule el pago total por alquilar una casa.
+        // El costo del alquiler se basa en los siguientes precios unitarios:
+        // S/.30.00 por cada habitación,
+        // S/.10.00 por cada baño,
+        // S/.15.00 por cada patio.
+        // El usuario debe ingresar la cantidad de habitaciones, baños y patios,
+        // y el programa debe mostrar el pago total.
+
+        int habitaciones, baños, patios;
+        double pagoTotal;
+
+        Console.WriteLine("Ingrese la cantidad de habitaciones:");
+        habitaciones = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la cantidad de baños:");
+        baños = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la cantidad de patios:");
+        patios = Convert.ToInt32(Console.ReadLine());
+
+        pagoTotal = (habitaciones * 30) + (baños * 10) + (patios * 15);
+
+        Console.WriteLine("El pago total por el alquiler es: S/. " + pagoTotal);
     }
 }
